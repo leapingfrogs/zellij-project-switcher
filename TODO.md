d283c850bafe025534be486aa9b69f78f5bc9280
Ian Davies <idavies@leapingfrogs.com>
2024-06-11 00:33:43 +0100

    Spike Continues
    Now able to invoke a command and read back the output, selecting the
    first project by default and switching to it.

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+- [ ] Populate the state model with the returned project names
+- [ ] Populate the UI with the returned project names
+- [ ] Allow selecting a project name in the UI
+- [ ] Allow filtering the project names by typing in the UI
+- [ ] Allow specifying confituration or the project query filter
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,155 @@
 use ansi_term::{Colour::Fixed, Style};
 use zellij_tile::prelude::*;
 
-use std::collections::{BTreeMap, HashMap};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::{collections::BTreeMap, process};
 
 #[derive(Default)]
 struct State {
-    mode_log: HashMap<String, usize>,
-    tabs: Vec<String>,
-    test_runs: usize,
     userspace_configuration: BTreeMap<String, String>,
 }
 
+#[derive(Default, Serialize, Deserialize)]
+pub struct ProjectWorker {}
+
 register_plugin!(State);
+
+register_worker!(ProjectWorker, project_worker, PROJECT_WORKER);
+
+impl ZellijWorker<'_> for ProjectWorker {
+    fn on_message(&mut self, message: String, payload: String) {
+        eprintln!("worker message: {:?} payload: {:?}", message, payload);
+        if message == "list_projects" {
+            let output = process::Command::new("fd")
+                .args([
+                    "-Htd",
+                    "^\\.git$",
+                    "/Users/idavies",
+                    "/Users/idavies/Documents",
+                    "/Users/idavies/Documents/GitHub",
+                    "/Users/idavies/Documents/GitHub/Github",
+                    "--max-depth=2",
+                ])
+                .output()
+                .expect("failed to execute process");
+            let result = match std::str::from_utf8(&output.stdout) {
+                Ok(val) => val,
+                Err(_) => "No Projects found",
+            };
+            eprintln!("Got projects: {}", result);
+            post_message_to_plugin(PluginMessage {
+                name: "projects".into(),
+                payload: result.into(),
+                worker_name: None,
+            })
+        }
+    }
+}
 
 impl ZellijPlugin for State {
     fn load(&mut self, configuration: BTreeMap<String, String>) {
         self.userspace_configuration = configuration;
         // we need the ReadApplicationState permission to receive the ModeUpdate and TabUpdate
         // events
-        // we need the RunCommands permission to run "cargo test" in a floating window
+        // we need the ChangeApplicationState permission to open sessions
         request_permission(&[
             PermissionType::ReadApplicationState,
+            PermissionType::ChangeApplicationState,
             PermissionType::RunCommands,
         ]);
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate, EventType::Key]);
+        subscribe(&[
+            EventType::Key,
+            EventType::CustomMessage,
+            EventType::RunCommandResult,
+        ]);
     }
     fn pipe(&mut self, pipe_message: PipeMessage) -> bool {
         eprintln!("pipe_message: {:?}", pipe_message);
         true
     }
     fn update(&mut self, event: Event) -> bool {
-        let mut should_render = false;
+        let mut should_render = true;
         match event {
-            Event::ModeUpdate(mode_info) => {
-                let mode = format!("{:?}", mode_info.mode);
-                let count = self.mode_log.entry(mode).or_insert(0);
-                *count += 1;
-                should_render = true;
-            }
-            Event::TabUpdate(tab_info) => {
-                self.tabs = tab_info.iter().map(|t| t.name.clone()).collect();
+            Event::CustomMessage(message, payload) => {
+                eprintln!("custom_message: {:?} payload: {:?}", message, payload);
                 should_render = true;
             }
             Event::Key(key) => {
-                if let Key::Char('n') = key {
-                    self.test_runs += 1;
-                    open_command_pane_floating(
-                        CommandToRun {
-                            path: "cargo".into(),
-                            args: vec!["test".to_owned()],
-                            cwd: None,
-                        },
-                        None,
+                if let Key::Char('N') = key {
+                    switch_session_with_layout(
+                        Some("Downloads".into()),
+                        LayoutInfo::BuiltIn("default".into()),
+                        Some("/Users/idavies/Downloads".into()),
                     );
                 }
+                if let Key::Char('w') = key {
+                    post_message_to(PluginMessage {
+                        name: String::from("list_projects"),
+                        payload: String::from("sample_payload"),
+                        worker_name: Some(String::from("project")),
+                    });
+                }
+                if let Key::Char('g') = key {
+                    let mut options = BTreeMap::new();
+                    options.insert("sample".into(), "42".into());
+                    run_command(
+                        &[
+                            "fd",
+                            "-Htd",
+                            "^\\.git$",
+                            "/Users/idavies",
+                            "/Users/idavies/Documents",
+                            "/Users/idavies/Documents/GitHub",
+                            "/Users/idavies/Documents/GitHub/Github",
+                            "--max-depth=2",
+                        ],
+                        options,
+                    );
+                }
+                should_render = true;
+            }
+            Event::RunCommandResult(Some(status), stdin, stdout, data) => {
+                eprintln!(
+                    "Command Result: status: {:?} in: {:?} out: {:?} data: {:?}",
+                    status,
+                    match std::str::from_utf8(&stdin) {
+                        Ok(val) => {
+                            split_lines(val).into()
+                        }
+                        Err(_) => {
+                            "None".to_string()
+                        }
+                    },
+                    match std::str::from_utf8(&stdout) {
+                        Ok(val) => val,
+                        Err(_) => "No Projects found",
+                    },
+                    data
+                );
+                if status == 0 {
+                    let result = match std::str::from_utf8(&stdin) {
+                        Ok(val) => do_lines(val),
+                        Err(_) => {
+                            vec![]
+                        }
+                    };
+                    match result.first() {
+                        Some(l) => {
+                            eprintln!("Would open: {}", l);
+                            match l.split_once("::") {
+                                Some((name, cwd)) => switch_session_with_layout(
+                                    Some(name.into()),
+                                    LayoutInfo::BuiltIn("default".into()),
+                                    Some(cwd.into()),
+                                ),
+                                None => {}
+                            }
+                        }
+                        None => {}
+                    }
+                }
+                should_render = true;
             }
             _ => (),
         };
@@ -64,34 +160,14 @@ impl ZellijPlugin for State {
         let colored_rows = color_bold(CYAN, &rows.to_string());
         let colored_cols = color_bold(CYAN, &cols.to_string());
         println!("");
-        println!(
-            "I quite like {} rows and {} columns",
-            colored_rows, colored_cols
-        );
+        println!("Size: {} rows and {} columns", colored_rows, colored_cols);
         println!("");
         println!(
             "{} {:#?}",
-            color_bold(
-                GREEN,
-                "I was started with the following user configuration:"
-            ),
+            color_bold(GREEN, "Started with the following user configuration:"),
             self.userspace_configuration
         );
         println!("");
-        println!("{}", color_bold(GREEN, "Modes:"));
-        for (mode, count) in &self.mode_log {
-            let count = color_bold(ORANGE, &count.to_string());
-            println!("{} -> Changed {} times", mode, count);
-        }
-        println!("");
-        let current_tabs = color_bold(GREEN, "Current Tabs:");
-        let comma = color_bold(ORANGE, ", ");
-        println!("{} {}", current_tabs, self.tabs.join(&comma));
-        println!("");
-        if self.test_runs > 0 {
-            let test_run_count = color_bold(CYAN, &self.test_runs.to_string());
-            println!("Ran tests {} times!", test_run_count);
-        }
     }
 }
 
@@ -106,4 +182,26 @@ pub const ORANGE: u8 = 166;
 
 fn color_bold(color: u8, text: &str) -> String {
     format!("{}", Style::new().fg(Fixed(color)).bold().paint(text))
+}
+
+fn do_lines(lines: &str) -> Vec<String> {
+    return lines.lines().map(|l| split(l)).collect();
+}
+
+fn split_lines(lines: &str) -> String {
+    return lines
+        .lines()
+        .fold("".to_string(), |acc, l| {
+            format!("{},{}", acc, split(l).to_owned())
+        })
+        .into();
+}
+
+fn split(line: &str) -> String {
+    let re = Regex::new(r"^(?<path>.*\/(?<name>[^/]+))\/.git\/$").unwrap();
+
+    match re.captures(line) {
+        Some(caps) => format!("{}::{}", &caps["name"], &caps["path"]),
+        None => "default".into(),
+    }
 }
